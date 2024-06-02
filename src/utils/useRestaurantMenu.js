f5f3afb721cee712e524c7d6178ba5f17e3306ec
shubham shahane <shubhamshahane41@gmail..com>
2024-06-02 21:52:06 +0530

    api changed

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -4,10 +4,7 @@ const useRestaurantMenu = (resId) => {
 
   const restaurantMenufn = async () => {
     const fetchMenu = await fetch(
-      "https://corsproxy.org/?" +
-        encodeURIComponent(
-          `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5946784&lng=73.7095365&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
-        )
+      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5946784&lng=73.7095365&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
     );
     const RestaurantMenuData = await fetchMenu.json();
     setresInfo(RestaurantMenuData?.data);
